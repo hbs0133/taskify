1d8567d3821b03b80cbc9ae019540301ce2e5b99
hbs0133 <hbs0133@naver.com>
2024-08-13 23:14:23 +0900

    feat: OG태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang='ko'>
       <Head>
+        <title>SCHEDO</title>
+        <meta property='og:title' content='SCHEDO' />
+        <meta
+          property='og:description'
+          content='스마트하게 나의 일정을 관리해보자!'
+        />
+        <meta property='og:url' content='https://schedo-7-6.netlify.app/' />
+        <meta property='og:image' content='/og_image.png' />
         <link rel='icon' href='/favicon.png' />
       </Head>
       <body>
